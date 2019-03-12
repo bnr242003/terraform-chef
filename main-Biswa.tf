d8ef19211d866f98fcb5c04eae81230675158342
bnr242003 <48213351+bnr242003@users.noreply.github.com>
2019-03-12 18:15:59 -0400

    Update main-Biswa.tf

--- main-Biswa.tf
+++ main-Biswa.tf
@@ -9,9 +9,10 @@ resource "aws_instance" "test_jenkins_instance"{
   subnet_id = "subnet-0faca3ecb7f953440"
   instance_initiated_shutdown_behavior = "terminate"
   key_name = "Biswanew"
-
-   tags {
+  tags {
     Name = "testjenkins"
     }
-
+provisioner "local-exec"{
+  command = "echo ${aws_instance.test_jenkins_instance.public_ip} >> /tmp/public_ip.txt"
+  }
 }
