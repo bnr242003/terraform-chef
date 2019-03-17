aa92e7147dd45bbc359a72f97515324e9bf455e8
bnr242003 <48213351+bnr242003@users.noreply.github.com>
2019-03-17 19:44:11 -0400

    Update var.tf

--- var.tf
+++ var.tf
@@ -6,10 +6,6 @@ variable "subnet-id-aws" {
   default = "subnet-0faca3ecb7f953440"
 }
 
-variable "sg-id-aws" {
-  default = "sg-0a1548c50229482ee"
-}
-
 variable "ami-aws" {
   default = "ami-0080e4c5bc078760e"
 }
