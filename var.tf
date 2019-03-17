abdd9663e4a827beae433394f97cd7f2c7963301
bnr242003 <48213351+bnr242003@users.noreply.github.com>
2019-03-17 19:00:03 -0400

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,3 +1,15 @@
-variable "instancefile"{
-  default ="10.0.0.0"
+variable "region-aws" {
+  default = "us-east-1"
+}
+
+variable "subnet-id-aws" {
+  default = "subnet-991258fc"
+}
+
+variable "sg-id-aws" {
+  default = "sg-0a236475"
+}
+
+variable "ami-aws" {
+  default = "ami-0080e4c5bc078760e"
 }
