49a893037087f9591c8f303a686dd0c8fc5b5678
bnr242003 <48213351+bnr242003@users.noreply.github.com>
2019-03-18 10:37:41 -0400

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,11 +1,11 @@
 variable "region_test" {
-  default = "${TF_VAR_region_test}"
+  region_test = "${TF_VAR_region_test}"
 }
 
 variable "subnet-id_test" {
-  default = "${TF_VAR_subnet-id_test}"
+  subnet-id_test = "${TF_VAR_subnet-id_test}"
 }
 
 variable "ami_test" {
-  default = "${TF_VAR_ami_test=ami}"
+  ami_test = "${TF_VAR_ami_test=ami}"
 }
