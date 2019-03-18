4b27dd117d072ce1f8b1070f7771397eb05b5e5e
bnr242003 <48213351+bnr242003@users.noreply.github.com>
2019-03-18 10:48:02 -0400

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,11 +1,11 @@
-variable "region_test" {
+variable "TF_VAR_region_test" {
  
 }
 
-variable "subnet-id_test" {
+variable "TF_VAR_subnet-id_test" {
   
 }
 
-variable "ami_test" {
+variable "TF_VAR_ami_test" {
 
 }
