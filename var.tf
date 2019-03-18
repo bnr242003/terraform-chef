0ce389278b4a74c703a768b567dbb95421e036a7
bnr242003 <48213351+bnr242003@users.noreply.github.com>
2019-03-17 22:18:12 -0400

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,11 +1,11 @@
 variable "region_test" {
-  region_test = "${$BUILD_REGION}"
+  default = "${$BUILD_REGION}"
 }
 
 variable "subnet-id_test" {
-  subnet-id_test = "${$BUILD_SUBNET_ID}"
+  default = "${$BUILD_SUBNET_ID}"
 }
 
 variable "ami_test" {
-  ami_test = "${$BUILD_AMI}"
+  default = "${$BUILD_AMI}"
 }
